4ec0da56785a77b5b9efa8a7e41b85c3e2351de4
Abiodun Oni <abiodun.oni@bankly.ng>
2024-01-04 07:02:33 +0100

    first class on variables and string

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+// variables in swift
+var x: Int = 3
+var y :Int = 4
+var z: Int = x+y
+var a : Float = 3.7
+var myName : String = "Abbey"
+var    age : String = "40"
+
+
+//ma nipulating Stringds
+
+var my2ndName : String = "Developer O"
+var  mAge : String = "20"
+var myinfo :String = my2ndName + mAge
+var checkValuesIString :String = "final print values in string are \(a) \(x)"
+
+print(checkValuesIString.count)
+var person1 : String = ""
+var person2 : String = ""
+var adjective1 : String = ""
+var adjective2: String = ""
+var verb1 : String = ""
+var verb2 : String = ""
+
+
+var storyTemplate = "The |(person)"
